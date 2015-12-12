b3cd08788c0d6d8b1db2ee0d0f612c6c521d4bd7
unknown <johnynogueira1@gmail.com>
2015-12-11 22:55:31 -0200

    template strings

---
+++ compiled/template_string.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var world = "world";
+
+console.log("hello " + world + "!");
+
+var foo = 1;
+var bar = 'string y';
+
+var text = "multiple lines break:\nvalue of foo = " + foo + "\nand value of bar = \"" + bar + "\".";
+
+console.log(text);
+
+console.log("Simple sum of 7 + 1 is " + (7 + 1));
+
+var s1 = 2;
+var s2 = 3;
+
+console.log("2 * 3 is " + s1 * s2);
